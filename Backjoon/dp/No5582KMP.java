5e2b00c4b9df4e3f66d85ea86ec7cb8e215e1e50
박철민 <fe26ming@gmail.com>
2022-04-23 23:24:26 +0900

    최장 부분 증가 수열

---
+++ Backjoon/dp/No5582KMP.java
@@ -0,0 +1,47 @@
+package backjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No5582KMP {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String a = br.readLine();
+		String b = br.readLine();
+
+		int alen = a.length();
+		int blen = b.length();
+
+		int pi[] = new int[blen];
+		
+		for(int i=1, j=0; i<blen; i++) {
+			while(j>0 && b.charAt(i) != b.charAt(j))
+				j = pi[j-1];
+			if(b.charAt(i) == b.charAt(j))
+				pi[i] = ++j;
+		}
+		
+		int j=0;
+		int result = 0;
+		for(int i=0; i<alen; i++) {
+			while(j > 0 && a.charAt(i) != b.charAt(j)) { 
+				j = pi[j-1];
+			}
+			
+			if(a.charAt(i) == b.charAt(j)) {
+				if(j == blen - 1) {
+					result = 1;
+					break;
+				}
+				else
+					++j;
+			}
+			if(result ==1) break;
+		}
+		System.out.println(result);
+		br.close();
+	}
+}
